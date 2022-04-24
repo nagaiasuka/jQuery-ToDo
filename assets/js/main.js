c87e413091a7439374dc7b8f80998cea9d08b379
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-04-24 16:47:39 +0900

    要素の取得・追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,18 @@ $(function() {
     // ボタン取得
     const btn = $("#btn");
     
+    $("#btn").on("click", function(){
+
+        // 要素の取得
+        let value = $("#input").val();
+        console.log(value);
+        // 追加箇所
+        let ul = $(".todo-list");
+        // 追加するもの
+        let addNode1 =$("<li>").text(value);
+        ul.append(addNode1);
+       
+    });
 
 
-    
 });
