6932559c94e43bdef7f8739dcb728c39427ba12e
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-04-24 17:23:12 +0900

    削除機能実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,28 @@
 $(function() {
-    // ボタン取得
-    const btn = $("#btn");
     
     $("#btn").on("click", function(){
 
         // 要素の取得
         let value = $("#input").val();
-        console.log(value);
-        // 追加箇所
-        let ul = $(".todo-list");
-        // 追加するもの
-        let addNode1 =$("<li>").text(value);
-        let deleteBtn =$("<div>").addClass("delete").text("削除");
+
+        if(value != ""){
+            
+            // 追加箇所
+            let ul = $(".todo-list");
+            // 追加するもの
+            let addNode1 =$("<li>").text(value);
+            let deleteBtn =$("<div>").addClass("delete").text("削除");
+            
+            ul.append(addNode1);
+            $(addNode1).append(deleteBtn);
+
+            
+        }
         
-        ul.append(addNode1);
-        $(addNode1).append(deleteBtn);
-       
+        // 削除機能
+        $(".delete").on("click", function(){
+            $(this).parent().remove();
+        });
     });
 
 
